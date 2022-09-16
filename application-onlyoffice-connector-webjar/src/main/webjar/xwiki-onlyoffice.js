7eee90d62b0f3b57fc0243cbe02f1a22c1f05faf
Teodor Caras <trrenty@gmail.com>
2022-09-16 17:40:17 +0300

    Sometimes after editing a file and clicking Save & View a "Leave site?" message box appears #30
    * Replace ternary operator with a cleaner line of code.

--- application-onlyoffice-connector-webjar/src/main/webjar/xwiki-onlyoffice.js
+++ application-onlyoffice-connector-webjar/src/main/webjar/xwiki-onlyoffice.js
@@ -262,7 +262,9 @@ define(['jquery'], function ($) {
         onReady: onReady,
         onDownloadAs: function (evt) { saveCallbackOO(evt.data.url || evt.data); },
         onRequestEditRights: function () { docEditor.applyEditRights(true); },
-        onDocumentStateChange: function (evt) { evt.data ? $('#button-sav').prop('disabled', true) : $('#button-sav').prop('disabled', false); },
+        // The event.data will be true when the current user is editing the document and false when the current user's
+        // changes are sent to the document editing service.
+        onDocumentStateChange: function (evt) { $('#button-sav').prop('disabled', evt.data); },
         onError: onError
       }
     });
